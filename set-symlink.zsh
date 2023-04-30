c26fea17c9666213a7a996d1c1bca53db86d850f
lizhi <liiil825.2012@gmail.com>
2023-04-30 22:49:31 +0800

    chore: add soft link

--- set-symlink.zsh
+++ set-symlink.zsh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
 
 local dir=$(pwd)
+mkdir -p $HOME/.config
+ln -sf $dir/config/zsh $HOME/.config
 ln -sf $dir/config/zsh/zprofile.symlink $HOME/.zprofile
 ln -sf $dir/config/zsh/zshenv.symlink $HOME/.zshenv
 ln -sf $dir/config/zsh/zshrc.symlink $HOME/.zshrc
+
+ln -sf $dir/config/tmux $HOME/.config
