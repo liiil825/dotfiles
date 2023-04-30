2621973ee3da77ad3fcf095e84204d7d0e3d56e8
lizhi <liiil825.2012@gmail.com>
2023-04-30 21:14:23 +0800

    chore: clean

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,8 @@
-starship init fish | source
+starship init fish | source # https://starship.rs/
+zoxide   init fish | source # 'ajeetdsouza/zoxide'
+
+# set -gx RIPGREP_CONFIG_PATH "$HOME/.config/rg/ripgreprc"
+# set -U FZF_DEFAULT_COMMAND "fd -H -E '.git'"
+set -Ux GOPATH (go env GOPATH)
+# set -Ux EDITOR nvim # 'neovim/neovim' text editor 
+# set -Ux VISUAL nvim
