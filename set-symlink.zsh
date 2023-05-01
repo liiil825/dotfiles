d2efe547b2d28fa08b4418f8f99ed5f6ec9ed66c
lizhi <liiil825.2012@gmail.com>
2023-05-01 16:50:58 +0800

    chore: insatll zsh requirement

--- set-symlink.zsh
+++ set-symlink.zsh
@@ -2,6 +2,7 @@
 
 hash -d config=$HOME/.config
 hash -d tplugs=~config/tmux/plugins
+hash -d repos=$HOME/repos
 
 local dir=$(pwd)
 mkdir -p ~config
@@ -17,4 +18,31 @@ if [[ ! -d ~tplugs/tpm ]]; then
     echo Download tmux plugin manager
     mkdir -p ~tplugs
     git clone --depth 1 https://github.com/tmux-plugins/tpm.git ~tplugs/tpm
+fi
+
+local blue="\033[36;1;4m"
+local close="\033[0m"
+
+if ! command -v sharship >/dev/null 2>&1; then
+    echo -e "$blueinstall sharship$close"
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
+if ! command -v zoxide >/dev/null 2>&1; then
+    echo -e "$blueinstall zoxid$close"
+    if [[ $(uname) == "Linux" ]]; then
+        curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    elif [[ $(uname) == "Darwin" ]]; then
+        brew install zoxide
+    fi
+fi
+
+if ! command -v fzf >/dev/null 2>&1; then
+    echo -e "$blueinstall fzf$close"
+    if [[ $(uname -n) == "archlinux" ]]; then
+        sudo pacman -S fzf
+    else
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~repos/fzf
+        ~repos/fzf/install
+    fi
 fi
