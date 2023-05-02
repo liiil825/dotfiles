6e3fbcc9c483560949a55f29979b1c0234b5048e
lizhi <liiil825.2012@gmail.com>
2023-05-02 22:40:28 +0800

    fix(zsh): pyenv for openclouds
    fix

--- config/zsh/config/pyenv.zsh
+++ config/zsh/config/pyenv.zsh
@@ -1,3 +1,7 @@
+#!/usr/bin/env zsh
+
+[[ $(uname -n) == *"-opencloudos" ]] && return
+
 # python pyenv
 export PYENV_ROOT=$HOME/.pyenv
 command -v pyenv >/dev/null || export PATH=$PATH:$PYENV_ROOT/bin
