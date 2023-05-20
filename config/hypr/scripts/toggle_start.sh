88b0bef07361323fe570cc0de56c7c74733a7e8f
lizhi <liiil825.2012@gmail.com>
2023-05-20 11:40:10 +0800

    feat(foot): fix the foot configuration file foot.init

--- config/hypr/scripts/toggle_start.sh
+++ config/hypr/scripts/toggle_start.sh
@@ -7,17 +7,19 @@ if [ -z "$1" ]; then
 fi
 
 # check if the process is running
+# use pgrep to check for emacs instead of emacsclient
 if pgrep -x "$1" >/dev/null; then
   # if it is running, kill it
-  echo "$1 is running, killing it now."
-
   case "$1" in
-  emacsclient)
+  emacs | emacsclient)
     # save all changes and kill emacsclient safely
-    # emacsclient -e '(save-buffers-kill-emacs)'
+    echo "Emacs is running, killing it now."
     emacsclient -s doom -e "(progn (save-some-buffers t) (save-buffers-kill-terminal))"
+    # emacsclient -s doom -e "(progn (save-some-buffers t) (save-buffers-kill-emacs))"
+    # emacsclient -s doom -e "(progn (save-some-buffers t) (make-frame-invisible nil t))"
     ;;
   *)
+    echo "$1 is running, killing it now."
     pkill -x "$1"
     ;;
   esac
@@ -26,20 +28,21 @@ else
   case "$1" in
   alacritty | kitty)
     echo "$1 is not running, starting it now."
-    $1 -e bash -c "
-      if tmux list-sessions &> /dev/null; then
-          tmux attach-session;
-      else
-          cd ~/Repos/dotfiles && tmux new -s dotfiles;
-            fi" &
+    $1 -e zsh -ic "
+       if tmux list-sessions &> /dev/null; then
+         tmux attach-session;
+       else
+         cd ~/Repos/dotfiles;
+         tmux new -s dotfiles;
+            fi"
     ;;
-  emacsclient)
+  emacs | emacsclient)
     echo "$1 is not running, starting it now."
-    emacsclient -c -s doom -a emacs ~/org/todo.org &
+    emacsclient -c -s doom -a emacs ~/org/todo.org
     ;;
   *)
     echo "$1 is not running, starting it now."
-    $1 &
+    $1
     ;;
   esac
 fi
